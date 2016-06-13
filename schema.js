39beaaeb33facb0d7d8049e1ca6d29c507a9c8da
Poga Po <poga.bahamut@gmail.com>
2016-06-14 01:43:49 +0800

    add schema.js

---
+++ schema.js
@@ -0,0 +1,128 @@
+var schema = [
+    {
+        type: "text",
+        name: "author",
+        placeholder: "you",
+        label: "Author",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "status",
+        placeholder: "planning",
+        label: "Status",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "name",
+        placeholder: "project name",
+        label: "Name",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "name_zh",
+        placeholder: "project name in zh",
+        label: "中文名稱",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "description",
+        placeholder: "description",
+        label: "Description",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "description_zh",
+        placeholder: "description_zh",
+        label: "中文敘述",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "homepage",
+        placeholder: "homepage url",
+        label: "專案網址",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "thumbnail",
+        placeholder: "thumbnail",
+        label: "thumbnail",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "document",
+        placeholder: "document",
+        label: "document",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "repository",
+        placeholder: "repository url",
+        label: "repository",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "licenses",
+        placeholder: "licenses",
+        label: "licenses",
+        optional: false,
+        array: true
+    },
+    {
+        type: "text",
+        name: "keywords",
+        placeholder: "foo, bar",
+        label: "keywords",
+        optional: false,
+        array: true
+    },
+    {
+        type: "text",
+        name: "audience",
+        placeholder: "public",
+        label: "audience",
+        optional: false,
+        array: true
+    },
+    {
+        type: "text",
+        name: "products",
+        placeholder: "urls...",
+        label: "products",
+        optional: false,
+        array: true
+    },
+    {
+        type: "text",
+        name: "partOf",
+        placeholder: "parent project",
+        label: "Part Of",
+        optional: false
+    },
+    {
+        type: "text",
+        name: "contributors",
+        placeholder: "contributors...",
+        label: "contributors",
+        optional: false,
+        array: true
+    },
+    {
+        type: "text",
+        name: "needs",
+        placeholder: "needs...",
+        label: "Needs",
+        optional: false,
+        array: true
+    },
+]
+
