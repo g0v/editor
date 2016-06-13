1564cb16d0d1ece8525b18ab54a603959cd13cc6
Poga Po <poga.bahamut@gmail.com>
2016-06-14 01:43:14 +0800

    extract schema to another file, cleanup

--- index.js
+++ index.js
@@ -1,131 +1,3 @@
-var schema = [
-    {
-        type: "text",
-        name: "author",
-        placeholder: "you",
-        label: "Author",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "status",
-        placeholder: "planning",
-        label: "Status",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "name",
-        placeholder: "project name",
-        label: "Name",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "name_zh",
-        placeholder: "project name in zh",
-        label: "中文名稱",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "description",
-        placeholder: "description",
-        label: "Description",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "description_zh",
-        placeholder: "description_zh",
-        label: "中文敘述",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "homepage",
-        placeholder: "homepage url",
-        label: "專案網址",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "thumbnail",
-        placeholder: "thumbnail",
-        label: "thumbnail",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "document",
-        placeholder: "document",
-        label: "document",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "repository",
-        placeholder: "repository url",
-        label: "repository",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "licenses",
-        placeholder: "licenses",
-        label: "licenses",
-        optional: false,
-        array: true
-    },
-    {
-        type: "text",
-        name: "keywords",
-        placeholder: "foo, bar",
-        label: "keywords",
-        optional: false,
-        array: true
-    },
-    {
-        type: "text",
-        name: "audience",
-        placeholder: "public",
-        label: "audience",
-        optional: false,
-        array: true
-    },
-    {
-        type: "text",
-        name: "products",
-        placeholder: "urls...",
-        label: "products",
-        optional: false,
-        array: true
-    },
-    {
-        type: "text",
-        name: "partOf",
-        placeholder: "parent project",
-        label: "Part Of",
-        optional: false
-    },
-    {
-        type: "text",
-        name: "contributors",
-        placeholder: "contributors...",
-        label: "contributors",
-        optional: false,
-        array: true
-    },
-    {
-        type: "text",
-        name: "needs",
-        placeholder: "needs...",
-        label: "Needs",
-        optional: false,
-        array: true
-    },
-]
-
 var app = new Vue({
     el: '#app',
     data: {
@@ -162,7 +34,7 @@ var app = new Vue({
             })
             .fail(function (err) {
                 //handle error with err
-                console.log(err)
+                console.error(err)
             })
         },
 
@@ -189,7 +61,6 @@ var app = new Vue({
             var branch
             app.conn.get(`/repos/${x.login}/metadata-editor/git/refs/heads/master`)
             .then(function (currentHEAD) {
-                console.log(content)
                 return app.conn.get(currentHEAD.object.url)
             })
             .then(function (HEADCommit) {
@@ -211,14 +82,12 @@ var app = new Vue({
                 ]})})
             })
             .then(function (newTree) {
-                console.log('new tree', newTree.sha)
                 return app.conn.post(`/repos/${x.login}/metadata-editor/git/commits`, { data: JSON.stringify({
                     message: "update g0v.json",
                     tree: newTree.sha,
                     parents: [currentHEADCommit.sha]})})
             })
             .then(function (newCommit) {
-                console.log('newCommit', newCommit)
                 branch = `update-g0v-json-${Date.now()}`
                 return app.conn.post(`/repos/${x.login}/metadata-editor/git/refs`, { data: JSON.stringify({sha: newCommit.sha, ref: `refs/heads/${branch}`})})
             })
