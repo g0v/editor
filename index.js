5cd6f08c47b6ebe17ce530fbaecd64cb945419ae
Poga Po <poga.bahamut@gmail.com>
2016-06-18 13:34:31 +0800

    fork and create PR from forked repo

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// key from oauth.io
+const oauthKey = 'M-bBVCTcOy9vIq7TRkJoL17N6LQ'
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -7,7 +10,8 @@ var app = new Vue({
         logged_in: false,
         username: "",
         conn: undefined,
-        user: undefined
+        user: undefined,
+        repo: ""
     },
     created() {
         schema.forEach(function(x,i) {
@@ -17,10 +21,16 @@ var app = new Vue({
                 schema[i].prefill = prefill
             }
         })
+
+        // prefill repo
+        var prefill_repo = getParameterByName("repo")
+        if (prefill_repo !== "") {
+            this.repo = prefill_repo
+        }
     },
     methods: {
         login() {
-            OAuth.initialize('M-bBVCTcOy9vIq7TRkJoL17N6LQ')
+            OAuth.initialize(oauthKey)
             OAuth.popup('github').done(function(result) {
                 console.log(result)
                 app.conn = result
@@ -59,20 +69,26 @@ var app = new Vue({
             var currentHEADCommit
             var newBlob
             var branch
-            app.conn.get(`/repos/${x.login}/metadata-editor/git/refs/heads/master`)
+            var fork
+            app.conn.post(`/repos/${app.repo}/forks`)
+            .then(function (user_fork) {
+                fork = user_fork
+                // wait until fork complete
+                return app.conn.get(`/repos/${x.login}/${fork.name}/git/refs/heads/master`)
+            })
             .then(function (currentHEAD) {
                 return app.conn.get(currentHEAD.object.url)
             })
             .then(function (HEADCommit) {
                 currentHEADCommit = HEADCommit
-                return app.conn.post(`/repos/${x.login}/metadata-editor/git/blobs`, {data: JSON.stringify({content: content, encoding: "utf-8"})})
+                return app.conn.post(`/repos/${x.login}/${fork.name}/git/blobs`, {data: JSON.stringify({content: content, encoding: "utf-8"})})
             })
             .then(function (blob) {
                 newBlob = blob
                 return app.conn.get(currentHEADCommit.tree.url)
             })
             .then(function (currentHEADtree) {
-                return app.conn.post(`/repos/${x.login}/metadata-editor/git/trees`, {data: JSON.stringify({base_tree: currentHEADtree.sha, tree: [
+                return app.conn.post(`/repos/${x.login}/${fork.name}/git/trees`, {data: JSON.stringify({base_tree: currentHEADtree.sha, tree: [
                     {
                         path: "g0v.json",
                         mode: "100644",
@@ -82,17 +98,17 @@ var app = new Vue({
                 ]})})
             })
             .then(function (newTree) {
-                return app.conn.post(`/repos/${x.login}/metadata-editor/git/commits`, { data: JSON.stringify({
+                return app.conn.post(`/repos/${x.login}/${fork.name}/git/commits`, { data: JSON.stringify({
                     message: "update g0v.json",
                     tree: newTree.sha,
                     parents: [currentHEADCommit.sha]})})
             })
             .then(function (newCommit) {
                 branch = `update-g0v-json-${Date.now()}`
-                return app.conn.post(`/repos/${x.login}/metadata-editor/git/refs`, { data: JSON.stringify({sha: newCommit.sha, ref: `refs/heads/${branch}`})})
+                return app.conn.post(`/repos/${x.login}/${fork.name}/git/refs`, { data: JSON.stringify({sha: newCommit.sha, ref: `refs/heads/${branch}`})})
             })
             .then(function (newRef) {
-                return app.conn.post(`/repos/${x.login}/metadata-editor/pulls`, { data: JSON.stringify({
+                return app.conn.post(`/repos/${app.repo}/pulls`, { data: JSON.stringify({
                     title: "update g0v.json",
                     body: "this is a pull request from metadata-editor",
                     head: `${x.login}:${branch}`,
