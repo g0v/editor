b176a656f206ab71e871184b1024f78f5988ec16
shelling <navyblueshellingford@gmail.com>
2016-10-08 16:00:53 +0800

    Root component hold all html

--- javascripts/components/Root/index.js
+++ javascripts/components/Root/index.js
@@ -26,6 +26,16 @@ class Root extends Component {
 
   render() {
     return <div>
+      <div id="header">
+        <img src="logo.png"/>
+        <h1>g0v metadata editor</h1>
+        <p>
+          The form below will help you build a valid g0v.json file.
+          Fill out all of the required fields (marked by a star) and any others.
+          Next, you can click the buttons at the bottom of the page to render and create a Pull Request to target repository.
+        </p>
+      </div>
+
       { this.state.github ? <Form github={this.state.github} /> : <Login login={this.login.bind(this)} /> }
     </div>;
   }
