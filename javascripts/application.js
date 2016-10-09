ecde043e9f13fa0602dfb0de82d4400e9c408fd8
shelling <navyblueshellingford@gmail.com>
2016-10-09 22:43:20 +0800

    Use Root component by default

--- javascripts/application.js
+++ javascripts/application.js
@@ -11,6 +11,7 @@ import Search from "components/Search";
 const oauthKey  = 'M-bBVCTcOy9vIq7TRkJoL17N6LQ'
 const component = (
   <Router history={browserHistory}>
+    <Route path="*"      component={Root} />
     <Route path="editor" component={Root} />
     <Route path="search" component={Search} />
   </Router>
