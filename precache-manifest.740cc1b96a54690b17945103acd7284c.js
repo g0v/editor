d53650a487405fedb253716a2e6ac4e05b2f1ab2
chunyenHuang <chunyenHuang@users.noreply.github.com>
2020-04-28 05:30:25 +0000

    deploy: f93737bc58d57cecca2dc4a23e794372d03d49f7

---
+++ precache-manifest.740cc1b96a54690b17945103acd7284c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06d333e2797e08c7f95418ec9788e7e",
+    "url": "/editor/index.html"
+  },
+  {
+    "revision": "921db5a930d4766e59bf",
+    "url": "/editor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7aaba9b7d3bd17dbb4f",
+    "url": "/editor/static/js/2.e0441e79.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/editor/static/js/2.e0441e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921db5a930d4766e59bf",
+    "url": "/editor/static/js/main.2fac5053.chunk.js"
+  },
+  {
+    "revision": "bca9d0b9be4f23e80dc9",
+    "url": "/editor/static/js/runtime-main.aab08fbd.js"
+  }
+]);
